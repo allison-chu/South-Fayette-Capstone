0f415ed6f2eb8498104f019c542026b93dcbe3a1
“Allison <im.allison.chu@gmail.com>
2025-07-15 19:01:23 -0400

    changed port to 5001

--- api_server.py
+++ api_server.py
@@ -53,6 +53,6 @@ def recommendations():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))  # use PORT env or default to 5000
+    port = int(os.environ.get("PORT", 5001))  # use PORT env or default to 5000
     app.run(host="0.0.0.0", port=port, debug=True)
 
