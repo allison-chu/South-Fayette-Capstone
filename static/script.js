fd362d3e2dab64809258c367c858babe5bc2e9a7
kulinski-cmu <kulinski@andrew.cmu.edu>
2025-07-19 17:41:16 -0400

    changed the pathing

--- static/script.js
+++ static/script.js
@@ -289,7 +289,7 @@ function updateProfile(student) {
 }
 
 function categoryOnClick() {
-    window.location.href = "../South-Fayette-Capstone/templates/page-not-built.html";
+    window.location.href = "../templates/page-not-built.html";
 }
 
 document.querySelector(".category-card").addEventListener("click", categoryOnClick());
